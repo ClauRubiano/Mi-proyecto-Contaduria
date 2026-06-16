789dfcfb7218774e0bdfe6f4a2c3a2bfcec7d020
ClauRubiano <claudiabibianarubianolopez@gmail.com>
2026-06-15 19:16:45 -0500

    Actualizando host

--- academia/settings.py
+++ academia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)=1e4jq2o&)ft^o641ggsmb3#xma+pd1dqg#_f*7b0dyuy&t=g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mi-proyecto-contaduria.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
